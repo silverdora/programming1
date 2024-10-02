cee4b50c1f54b24f5bcb131c68625b261e4e05f8
Daria Gakhova <727546@student.inholland.nl>
2024-10-02 22:14:21 +0200

    week 1

---
+++ week1-demos/Demo1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Who are you, dudes? ");
+            string personalities = Console.ReadLine();
+            Console.WriteLine("it's Wednesday, my dudes " + personalities);
+        }
+    }
+}
+
+

e42c1abdcf43c3ab70a159043cbaa8587aea497c
Daria Gakhova <727546@student.inholland.nl>
2024-10-23 22:14:48 +0200

    trial exam but prettier

--- trial-exam/assignment5/Machine.cs
+++ trial-exam/assignment5/Machine.cs
@@ -1,55 +1,55 @@
 ﻿using System;
 namespace assignment5
 {
-	public class Machine
-	{
+    public class Machine
+    {
         //fields
         private int _numberOfWorkingHours; //backing field
         //properties
         public string Name { get; private set; } //read-only
-		
-		private string needOfMaintance;
+
+        private string needOfMaintance;
 
         public int NumberOfWorkingHours
-		{
-			get
-			{ return _numberOfWorkingHours; }
-			set
-			{
-				if (value >= 0)
+        {
+            get
+            { return _numberOfWorkingHours; }
+            set
+            {
+                if (value >= 0)
 
-				{
-					_numberOfWorkingHours = value;
-				}
+                {
+                    _numberOfWorkingHours = value;
+                }
             }
-		}
+        }
 
-		public bool NeedsMaintenance
-		{
-			get
-			{
-				return (NumberOfWorkingHours >= 10000);
-			}
-		}
+        public bool NeedsMaintenance
+        {
+            get
+            {
+                return (NumberOfWorkingHours >= 10000);
+            }
+        }
 
         //constructor
         public Machine(string name, int numberOfWorkingHours)
-		{
-			Name = name;
-			NumberOfWorkingHours = numberOfWorkingHours;
-		}
+        {
+            Name = name;
+            NumberOfWorkingHours = numberOfWorkingHours;
+        }
 
-		//methods
+        //methods
         public void DisplayInfo()
-		{
-			if (NeedsMaintenance)
-				needOfMaintance = "yes";
-			else
-				needOfMaintance = "no";
-			Console.WriteLine($"\nMachine name: {Name}\n" +
-				$"Number of working hours: {NumberOfWorkingHours}\n" +
-				$"Needs maintainance: {needOfMaintance}");
-		}
+        {
+            if (NeedsMaintenance)
+                needOfMaintance = "yes";
+            else
+                needOfMaintance = "no";
+            Console.WriteLine($"\nMachine name: {Name}\n" +
+                $"Number of working hours: {NumberOfWorkingHours}\n" +
+                $"Needs maintainance: {needOfMaintance}");
+        }
     }
 }
 
